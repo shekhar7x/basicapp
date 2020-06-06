75bd020e811b22285bc3a9a86504f2ee917d559d
shekhar.s <shekhar7x@gmail.com>
2020-06-06 18:06:16 +0530

    done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 http = require('http')
 console.log('start');
-http.createServer(function(request,response){
+let port = 3000;
+http.createServer(function (request, response) {
     console.log('request recieved');
     console.log(request.socket.Socket)
-    response.writeHeader(200,{'Content-Type':'text/html'});
+    response.writeHeader(200, { 'Content-Type': 'text/html' });
     response.end('<h1>Welcome Here !!!!!</h1>');
-}).listen(8080);
+}).listen(port, undefined, undefined, () => {
+    console.log("Server started on port:", port)
+});
